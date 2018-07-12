d8104116b01500de70bd5589c1976a8afe3f653e
laffingragons <akshaypatil,vit@gmail.com>
2018-07-12 17:55:44 +0530

    Jwt token genration and usser creation done for now

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -14,13 +14,205 @@ const UserModel = mongoose.model('User')
 
 let signUpFunction = (req, res) => {
 
-  
+  let validateUserInput = () => {
+
+    return new Promise((resolve, reject) => {
+
+        if(req.body.email){
+            if(!validateInput.Email(req.body.email)){
+
+                let apiResponse = response.generate(true, "Email does not met the requirement", 400, null);
+                reject(apiResponse);
+
+            }else if(check.isEmpty(req.body.password)){
+
+                let apiResponse = response.generate(true, "Password is missing", 400, null);
+                reject(apiResponse);
+
+            }else{
+
+                resolve(req);
+
+            }
+        }else{
+
+            logger.error('Feild Missing during User creation', 'userController: signUpFunction()', 5)
+            let apiResponse = response.generate(true, "One or more parameters is missing", 400, null);
+            reject(apiResponse);
+
+        }
+
+    })
+
+  }//end of validate user 
+
+  let createUser = () => {
+
+    return new Promise((resolve, reject) => {
+
+        UserModel.findOne({email: req.body.email})
+        .exec((err, retrivedUserDetails) =>{
+
+            if(err){
+                logger.error(err.message, "userController: createUser()", 10);
+                let apiResponse =  response.generate(true, "Failed to create user", 400, null);
+                reject(apiResponse);
+            }else if(check.isEmpty(retrivedUserDetails)){
+                console.log(req.body);
+                let newUser = new UserModel({
+                    userId: shortid.generate(),
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName || '',
+                    email: req.body.email.toLowerCase(),
+                    mobileNumber: req.body.mobileNumber,
+                    password: passwordLib.hashpassowrd(req.body.password),
+                    createdOn: time.now()
+                })
+
+                newUser.save((err, newUser) => {
+
+                    if(err){
+
+                        console.log(err);
+                        logger.error(err.message, "userController: CreateUser()", 400 , null)
+                        let apiResponse =  response.generate(true, "Failed to create user", 400, null);
+                        reject(apiResponse);
+
+                    }else{
+
+                        let newUserObj = newUser.toObject();
+                        resolve(newUserObj)
+
+                    }
+
+                })
+
+            }else{
+
+                logger.error('User Cannot be Created. User Already Present', "userController: createUser()", 5);
+                let apiResponse =  response.generate(true, "User Already Present", 400, null);
+                reject(apiResponse);
+            }
+
+        })
+
+    })
+
+  }//end of create user function
+
+  validateUserInput(req, res)
+  .then(createUser)
+  .then((resolve) => {
+      delete resolve.password
+      let apiResponse = respone.generate(false, 'User Created Successfully', 200, resolve );
+      res.send(apiResponse);
+  })
 
 }// end user signup function 
 
 // start of login function 
 let loginFunction = (req, res) => {
-    
+    let findUser = () => {
+        return new Promise ((resolve, reject) => {
+            if(req.body.email){
+
+                console.log('req.body email is there :', req.body.email);
+                UserModel.findOne({email: req.body.email},(err, userDetails) =>{
+
+                    if(err){
+                        console.log(err);
+                        logger.error('Failed to Retrive User Data', 'userController: findUser()', 5);
+                        let apiResponse = response.generate(true, 'Failed to Retrive User Data', 400, null);
+                        reject (apiResponse);
+
+                    }else if(check.isEmpty(userDetails)){
+                        logger.error('No user found', 'userController: findUser()', 5);
+                        let apiResponse = response.generate(true, 'No user found', 400, null);
+                        reject (apiResponse);
+                    }else{
+                        logger.info("User found", 'userController: findUser()', 10);
+                        resolve(userDetails);
+                    }
+
+                })
+
+            }else{
+                let apiResponse = response.generate(true, '"Email" parameter is missing', 400, null);
+                reject(apiResponse);
+            }
+        })
+
+    }
+
+    let validatePassword = (retrivedUserDetails) => {
+        console.log("ValidatePassword");
+        return new Promise ((resolve, reject) => {
+            passwordLib.comparePassword(req.body.password, retrivedUserDetails.password, (err, isMatch)=>{
+                if(err){
+                    console.log(err);
+                    logger.error(err.message, " userController: validatePassword()", 10);
+                    let apiResponse = response.generate(true, "login failed", 500, null);
+                    reject(apiResponse);
+                }else if(isMatch){
+                    let retrivedUserDetailsObj = retrivedUserDetails.toObject()
+                    delete retrivedUserDetailsObj.password
+                    delete retrivedUserDetailsObj._id
+                    delete retrivedUserDetailsObj.__v
+                    delete retrivedUserDetailsObj.createdOn
+                    delete retrivedUserDetailsObj.modifiedOn
+                    resolve(retrivedUserDetailsObj)
+                }else{
+                    logger.info('Password did not match', 'userController: findUser()', 10);
+                    let apiResponse = response.generate(true, 'Password did not match. Login Failed', 400, null);
+                    reject(apiResponse);
+                }
+
+            })
+
+        })
+    }
+
+    let generateToken = (userDetails) => {
+
+        console.log("Generate token");
+        return new Promise((resolve, reject)=> {
+            token.generateToken(userDetails, (err, tokenDetails) =>{
+
+                if(err){
+
+                    console.log(err);
+                    let apiResponse = response.generate(true, "Error in generating Jwt Token", 500, 7);
+                    reject(apiResponse);
+
+                }else{
+
+                    tokenDetails.userId = userDetails.userId,
+                    tokenDetails.userDetails = userDetails
+                    resolve(tokenDetails);
+
+                }
+            })
+        })
+    }
+
+    findUser(req, res)
+    .then(validatePassword)
+    .then(generateToken)
+    .then((resolve) => {
+
+        let apiResponse = response.generate(false, "Login Successful", 200, resolve);
+        res.status(200);    
+        res.send(apiResponse);
+
+    })
+    .catch((err) => {
+
+        console.log("Error Handler");
+        console.log(err);
+        res.status(err.status);
+        res.send(err);
+        
+    })
 }
 
 
---
+++ app/libs/tokenLib.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const shortId = require('shortid');
+const secretKey = "RandomString";
+
+let generateToken = (data, err) => {
+
+    try{
+
+        let claims = {
+
+            jwtId: shortId.generate(),
+            iat: Date.now(),
+            exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
+            sub: 'authToken',
+            iss: 'edChat',
+            data: data
+
+        }
+
+        let tokenDetails = {
+            token: jwt.sign(claims, secretKey),
+        }
+        cb(null, tokenDetails)
+    } catch(err){
+
+        console.log(err);
+        cb(err, null)
+        
+    }
+}
