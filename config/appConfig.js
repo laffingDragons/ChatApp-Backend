05a22d74b83965527d343bc4eaaf1d2b74a43215
laffingragons <akshaypatil,vit@gmail.com>
2018-08-10 14:24:56 +0530

    prod

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,7 +1,16 @@
 let appConfig = {};
 let nodeMailer = {};
 
+appConfig.port = 3000;
+appConfig.allowedCorsOrigin = "*";
+appConfig.env = "dev";
+appConfig.db = {
+    uri: 'mongodb://akshay:akshay12@ds119072.mlab.com:19072/chatapp'
+  }
+appConfig.apiVersion = '/api/v1';
 
+nodeMailer.email="cooldudeakshu@gmail.com";
+nodeMailer.password = "Mlg@420^";
 
 module.exports = {
     port: appConfig.port,
