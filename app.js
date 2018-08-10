318db1357d5a772e214260854bb90dfdaaaaaa2b
laffingragons <akshaypatil,vit@gmail.com>
2018-08-10 14:53:40 +0530

    solving erro

--- app.js
+++ app.js
@@ -66,7 +66,7 @@ app.use(globalErrorMiddleware.globalNotFoundHandler);
 
 const server = http.createServer(app);
 // start listening to http server
-server.listen(appConfig.port);
+server.listen(process.env.PORT);
 server.on('error', onError);
 server.on('listening', onListening);
 
